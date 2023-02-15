64e2dc256c5f6ab59a2c6cc604ebcd4a9cc22eac
diivanych <diivanych@list.ru>
2023-02-15 21:46:55 +0300

    конец задачи

--- Labs/Labs01/Program.cs
+++ Labs/Labs01/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное число из отрезка
+// [10, 99] и показывает наибольшую цифру чсла.
+
+// 78 -> 8    12 -> 2    85 -> 8
+
+void MaxDecimal(int num)
+{
+    int edc = num % 10;
+    int dec = num / 10;
+    if(edc > dec)
+    {
+        Console.WriteLine($"Большая цифра числа {num} -> {edc}");
+    }
+    else
+    {
+        Console.WriteLine($"Большая цифра числа {num} -> {dec}");
+    }
+}
+
+int randomnumber = new Random().Next(10, 100);
+MaxDecimal(randomnumber);
