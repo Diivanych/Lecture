ac00ea31396ee83e7d08cd439d176a27a5c44e00
diivanych <diivanych@list.ru>
2023-02-15 20:56:47 +0300

    example01

---
+++ Examples/Example04/Program.cs
@@ -0,0 +1,36 @@
+﻿// Создаём два метода
+
+// 1. Метод void FillArray()
+// заполняет массив случайными числами 
+
+// 2. Метод void PrintArray()
+// Выводит его на экран
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(100, 1000);
+        index++;
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    Console.WriteLine("Наш массив заполнен следующими элементами");
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
---
+++ Examples/Example05/Program.cs
@@ -0,0 +1,56 @@
+﻿// Поиск позиции заданного элемента в массиве
+// Создаём новый метод IndexOf()
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 10);
+        index++;
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    Console.WriteLine("Наш массив заполнен следующими элементами");
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -2;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+Console.WriteLine("Искомый элемент находится на позиции");
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos + 1);
