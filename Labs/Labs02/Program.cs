a614e2db1b56fe3c88d8892d283b98584f64efa2
diivanych <diivanych@list.ru>
2023-02-15 22:17:02 +0300

    новая верся

--- Labs/Labs02/Program.cs
+++ Labs/Labs02/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int MaxDecimal(int num)
+{
+    int edc = num % 10;
+    int dec = num / 10;
+
+    if(edc > dec) num = edc;
+    else num = dec;
+    return num;
+}
+
+int randomnumber = new Random().Next(10, 100);
+int maxnum = MaxDecimal(randomnumber);
+Console.WriteLine($"Наибольшая цифра числа {randomnumber} это {maxnum}.");
