cfa9102e3504abccc2d4a4bff9faa9dc97c7041f
diivanych <diivanych@list.ru>
2023-02-17 17:04:27 +0300

    Все лабы

--- Labs/Labs03/Program.cs
+++ Labs/Labs03/Program.cs
@@ -2,3 +2,16 @@
 // и удаляет из него вторую цифру
 // 456 -> 46,   782 -> 72,   918 -> 98
 // просмотр остановлен на 38 минуте 
+
+int DeleteDecimal(int num)
+{
+    int sot = num / 100;
+    int edc = num %  10;
+    return sot * 10 + edc;
+    
+}
+
+int randomnamber = new Random().Next(100, 1000);
+
+Console.WriteLine(randomnamber);
+Console.WriteLine(DeleteDecimal(randomnamber));
