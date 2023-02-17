38d8299387ec0a83e0142befa4d57c6a87dc06a6
diivanych <diivanych@list.ru>
2023-02-17 17:23:07 +0300

    вся домашка за раз

---
+++ Home/Work01/Program.cs
@@ -0,0 +1,46 @@
+﻿// Напишите программу, которая выводит третью цифру 
+// заданного числа, или сообщает, что третьей цифры нет.
+// 645 -> 4,    78 -> трерьей цифры нет,    32679 -> 6
+
+int NumberControl(int num, int pos)
+{
+    int ind = 1;
+    int div = 1;
+    if(num < 0) num = (num * (- 1));
+       
+    while(ind <= pos)
+    {
+        div = div * 10;
+        ind++;
+    }
+    
+    if(num < div)
+        return -1;
+        return num;
+}
+
+int SearchPosiiton(int num, int pos)
+{
+    int ind = 1;
+    int div = 1;
+
+    while(ind <= pos)
+    {
+        div = div * 10;
+        ind++;
+    }
+        
+    while(num >= div)
+        num = num / 10;
+    return (num % 10);
+}
+
+Console.WriteLine("Введите чсло");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите искомую позицию");
+int position = Convert.ToInt32(Console.ReadLine());
+
+int result = SearchPosiiton(NumberControl(number, position), position);
+if(result == -1) Console.WriteLine("В этом числе нет такой позиции");
+else    Console.WriteLine($"{number} -> {result}");
---
+++ Home/Work03/Program.cs
@@ -0,0 +1,38 @@
+﻿/* 
+Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели и проверяет, 
+является ли этот день выходным.
+*/
+
+void TestDay(int req)
+{
+    while((req > 7) || (req <1))
+    {
+        Console.WriteLine();
+        Console.WriteLine("Календарь Майя не обслуживаем!");
+        Console.WriteLine("Поробуйте ещё раз.");
+        Console.WriteLine();
+        req = Convert.ToInt32(Console.ReadLine());
+    }
+    if(req < 6)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Работайте негры! Солнце ещё высоко.");
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("Парам-пабабам-пабабам-пабабам");
+        Console.WriteLine("Парам-пабабам. По бабам пора!");
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine("Введите интересующий Вас день недели.");
+Console.WriteLine();
+
+int request = Convert.ToInt32(Console.ReadLine());
+
+TestDay(request);
