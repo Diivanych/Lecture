2c7c1fcbffaf7c1fbd54e49795101dec4098178f
diivanych <diivanych@list.ru>
2023-02-21 00:41:08 +0300

    Таблица кубов

---
+++ Home/Work13/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая принимает 
+на вход число (N) и выдаёт таблицу 
+кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+Console.WriteLine();
+Console.WriteLine("Введите натуральное чсло N");
+Console.Write("-> ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Таблица кубов для числа N = {n} будет следующей:");
+Console.WriteLine();
+for(int i = 1; i <= n; i++) Console.Write($"{Math.Pow(i, 3)}, ");
+Console.WriteLine();
+Console.WriteLine();
