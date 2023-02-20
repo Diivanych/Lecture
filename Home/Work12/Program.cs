ded662424f253826eddee63252eda01a02d0f9d9
diivanych <diivanych@list.ru>
2023-02-21 00:07:48 +0300

    Длинна отрезка

--- Home/Work12/Program.cs
+++ Home/Work12/Program.cs
@@ -1,18 +1,27 @@
-﻿Console.WriteLine();
+﻿/*
+Напишите программу, которая принимает 
+на вход координаты двух точек и находит 
+расстояние между ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+Console.WriteLine();
 Console.Write("Введите координату X точки A -> ");
 double xa = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("Введите координату X точки B -> ");
-double xb = Convert.ToDouble(Console.ReadLine());
 
 Console.Write("Введите координату Y точки A -> ");
 double ya = Convert.ToDouble(Console.ReadLine());
 
-Console.Write("Введите координату Y точки B -> ");
-double yb = Convert.ToDouble(Console.ReadLine());
-
 Console.Write("Введите координату Z точки A -> ");
 double za = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату X точки B -> ");
+double xb = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату Y точки B -> ");
+double yb = Convert.ToDouble(Console.ReadLine());
 
 Console.Write("Введите координату Z точки B -> ");
 double zb = Convert.ToDouble(Console.ReadLine());
