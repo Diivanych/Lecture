3ce5b37e1ba3039cfdf9ea483234983caf60eb69
diivanych <diivanych@list.ru>
2023-02-21 00:01:26 +0300

    Длинна отрезка

---
+++ Home/Work12/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine();
+Console.Write("Введите координату X точки A -> ");
+double xa = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату X точки B -> ");
+double xb = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату Y точки A -> ");
+double ya = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату Y точки B -> ");
+double yb = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату Z точки A -> ");
+double za = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите координату Z точки B -> ");
+double zb = Convert.ToDouble(Console.ReadLine());
+
+double length = Math.Sqrt(Math.Pow((xa - xb), 2) + Math.Pow((ya - yb), 2) + Math.Pow((za - zb), 2));
+
+Console.WriteLine();
+Console.WriteLine($"Итоговая длина отрезка будет равна = {length}");
+Console.WriteLine();
