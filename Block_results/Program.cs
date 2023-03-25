8254ac41c351b390147b7b679bc9f5d1dee3ee5f
diivanych <diivanych@list.ru>
2023-03-25 21:19:36 +0300

    окончательно

---
+++ Block_results/Program.cs
@@ -0,0 +1,42 @@
+﻿/* 
+Задача:
+
+Написать программу, которая из меющегося массива 
+строк формирует массив из строк, длина которых 
+меньше либо равна 3 символам.
+
+Массив задаётся на входе.
+*/
+
+string[] array = { "Мороз", "и", "солнце","день", "чудесный", "ещё", "ты", "дремлешь", "друг", "прелестный"};
+
+void MethodShowArray(string[] array)
+{
+    Console.Write($"Input array: ");
+    for (int i = 0; i < array.Length; i++) 
+    Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+string[] MethodCreateArray(string[] array)
+{
+    Console.Write($"Output array: ");
+    string[] shortarr = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            shortarr[i] = array[i];
+            Console.Write($"{shortarr[i]} ");
+        }
+    }
+    Console.WriteLine();
+    return shortarr;
+}
+
+Console.WriteLine();
+MethodShowArray(array);
+Console.WriteLine();
+string[] newArray = MethodCreateArray(array);
+Console.WriteLine();
+// MethodShowArray(newArray);
